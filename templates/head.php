017dcfed7d668bcd37aef7f1697c6ac7bb640311
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-02-12 12:22:20 -0600

    Fonts

--- templates/head.php
+++ templates/head.php
@@ -27,6 +27,10 @@ http://12southmusic.com/
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <link rel="shortcut icon" href="<?php echo of_get_option('favicon_upload'); ?>" />
 
+<!--Fonts-->
+<script type="text/javascript" src="//use.typekit.net/poa3kkc.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
   <!--JQUERY and Javascript files-->
   <script type="text/javascript" src="../assets/js/jquery.backstretch.min.js"></script>
 </head>
