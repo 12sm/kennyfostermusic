900d6dd66fea1d1a918b6847fa1ccbe2f9d2d532
michael <michael@12southmusic.com>
2014-03-14 17:19:00 -0500

    Update custom.php

--- lib/custom.php
+++ lib/custom.php
@@ -15,4 +15,33 @@ return '<iframe width="300" height="50" style="position: relative; display: bloc
 
 add_shortcode('bandcamp', 'music_player');
 
+define( 'UPLOADS', ''.'assets' );
+
+function bread_crumbs ($atts) {
+
+   extract(shortcode_atts(array(
+    'width' => '100%',
+   ), $atts));
+return yoast_breadcrumb('<div class="breadcrumbs">','</div>');
+}
+
+add_shortcode('breadcrumbs', 'bread_crumbs');
+
+
+add_action( 'plugins_loaded', 'my_cleanup', 12 ); // after they are registered
+function my_cleanup() {
+if ( !is_admin() ) {
+global $WP_Views;
+remove_action('wp_print_styles', array($WP_Views, 'add_render_css'));
+remove_action('wp_head', 'wpv_add_front_end_js');
+remove_action('wp_footer', 'wpcf_access_dependencies_render_js');
+wp_dequeue_style( 'views-pagination-style' );
+wp_dequeue_script( 'views-pagination-script' );
+wp_dequeue_script( 'wpv-date-front-end-script' );
+wp_dequeue_script( 'jquery-ui-datepicker' );
+wp_dequeue_script( 'jquery-ui-core' );
+wp_dequeue_style( 'jquery-ui-datepicker' );
+}
+}
+
 ?>
