ea0573d9de6b0bdc4321e9d1cdc9c72b42a24f30
michael <michael@12southmusic.com>
2014-03-14 16:06:22 -0500

    Add col to app.css

--- template-photos.php
+++ template-photos.php
@@ -4,5 +4,7 @@ Template Name: Photos Template
 */
 ?>
 
+<div class="col">
 <?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/content', 'page-photos'); ?>
+</div>
