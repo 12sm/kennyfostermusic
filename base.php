074e1ea34ede9cb7af7051474ce3d8fa3003a0b0
michael <michael@12southmusic.com>
2014-03-14 15:16:55 -0500

    Return 404

--- base.php
+++ base.php
@@ -19,9 +19,7 @@
  
   <div class="wrap container-fluid" role="document">
     <div class="content">
-      <main class="main col-sm-12" role="main">
-      
-      
+      <main class="main col-sm-12" role="main">     
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
       <!--<?php if (roots_display_sidebar()) : ?>
