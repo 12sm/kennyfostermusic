2e2b4070434c809d56d57cfd706b970492991149
michael <michael@12southmusic.com>
2014-03-16 12:48:00 -0500

    Add Home Slider

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,3 +1,13 @@
+<?php
+
+echo do_shortcode( '[layerslider id="2"]');
+echo do_shortcode('[wpv-view name="About Home"]');
+echo do_shortcode('[wpv-view name="Video Home"]');
+echo do_shortcode('[wpv-view name="Tour Home"]');
+echo do_shortcode('[wpv-view name="Blog Home"]');
+echo do_shortcode('[wpv-view name="Store Banner"]');
+
+?>
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
