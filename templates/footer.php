eab63cfc0892a20ae041ecdfd6948716e0a441d7
michael <michael@12southmusic.com>
2014-03-17 14:15:38 -0500

    Mobile updates for the footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,15 +1,12 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
     <div class="col-lg-12">
-      <!--<div class="row sidebar-footer">
-      	<?php dynamic_sidebar('sidebar-footer'); ?>
-      </div>-->
     
           <div class="row footer-nav">
-          	<div class="col-sm-6">
+          	<div class="col-sm-3 col-xs-12">
             	<p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></p>
             </div>
-            <div class="col-sm-6">  
+            <div class="col-sm-9 hidden-xs">  
 					<?php
                  if (has_nav_menu('footer_navigation')) :
                    wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
@@ -28,10 +25,10 @@
                     <li><a href="http://www.youtube.com/user/KennyFosterOFFICIAL"><i class="fa fa-youtube-square fa-2x"></i></a></li>
                     </ul>
               </div>
-            </div><!-- end row social-nav -->
+            </div>
         
 
-</div><!-- end col 12 -->
+</div>
       
     </div>
   </div>
@@ -39,11 +36,7 @@
 
 <?php wp_footer(); ?>
 
-
-
-
-
-<!-- Begin 12SM Network Analytics <!-->   
+  
   <script type="text/javascript">
 	var _gaq = _gaq || [];
   	_gaq.push(['_setAccount', 'UA-27814560-3']);
@@ -56,4 +49,3 @@
 	  	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  	})();
 	</script>
-  <!-- End 12SM Network Analytics <!--> 
