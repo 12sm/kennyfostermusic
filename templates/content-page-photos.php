1905b949d888b6d72dd22e2fecf091f0400721c6
michael <michael@12southmusic.com>
2014-03-16 21:53:00 -0500

    Add photos h1's

--- templates/content-page-photos.php
+++ templates/content-page-photos.php
@@ -1,9 +1,8 @@
-<div class="col">
+<h1>Official Photos</h1>
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
 
+<h1>Instagram</h1>
 <ul class="insta-grid"></ul>
-
-</div>
